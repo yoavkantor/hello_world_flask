33c5cb63895c5d20457580e030c35841a1f62f2a
Yoav Kantor <yoav.kantor@gmail.com>
2023-07-23 13:40:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from flask import Flask
+import threading
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello():
+def hello_world():
     return 'Hello, World!'
+
+def run_flask_app(app):
+    app.run(use_reloader=False)
+
+flask_thread = threading.Thread(target=run_flask_app, args=(app,))
+flask_thread.start()
